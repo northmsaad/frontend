8a4c85b0deaf2001f37aa67f61f7bcdf379b6ce8
northmsaad <msddr010@gmail.com>
2023-01-20 21:00:50 +0500

    final

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,9 +5,8 @@ import {
   FaGithub,
   FaLinkedin,
   FaFacebook,
-  FaLinkedinIn,
+  FaInstagram,
 } from "react-icons/fa";
-import { HiOutlineMail } from "react-icons/hi";
 import { BsFillPersonLinesFill } from "react-icons/bs";
 import Logo from "../assets/logo.png";
 import { Link } from "react-scroll";
@@ -115,9 +114,9 @@ const Navbar = () => {
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]">
             <a
               className="flex justify-between items-center w-full text-gray-300"
-              href="https://mail.google.com/mail/u/0/?tab=rm&ogbl#inbox"
+              href="https://www.instagram.com/ibn_riaz147/"
               target="blank">
-              Email <HiOutlineMail size={30} />
+              Instagram <FaInstagram size={30} />
             </a>
           </li>
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]">
