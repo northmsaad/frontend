a63c3307e4461338353a6cb497ea3ec30b5a8573
northmsaad <msddr010@gmail.com>
2023-01-20 20:52:19 +0500

    portfolio final

---
+++ src/data/data.js
@@ -0,0 +1,27 @@
+import Bank from "../assets/project/f1.png";
+import TravelandTOur from "../assets/project/f2.png";
+import ChatGPTWEB from "../assets/project/f3.png";
+
+export const data = [
+  {
+    id: 1,
+    name: "React JS Application",
+    image: Bank,
+    github: "https://github.com/northmsaad/Bank",
+    live: "https://bank-sable.vercel.app/",
+  },
+  {
+    id: 2,
+    name: "React JS Application",
+    image: TravelandTOur,
+    github: "https://github.com/northmsaad/Four-Season",
+    live: "https://four-season.vercel.app/",
+  },
+  {
+    id: 3,
+    name: "React JS Application",
+    image: ChatGPTWEB,
+    github: "https://github.com/northmsaad/GPT_3",
+    live: "https://gpt-3-pi-jade.vercel.app/",
+  },
+];
